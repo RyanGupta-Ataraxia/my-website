7af0fd41952ea27ac31765d150d87779b0e983d4
Ryan Gupta <ryangupta680@gmail.com>
2026-01-28 12:12:35 +0530

    Debugging js

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-alert("JS is connected");
-
 const toggleBtn = document.getElementById("theme-toggle");
-const body = document.body;
 
 toggleBtn.addEventListener("click", () => {
-    body.classList.toggle("light");
-
-    if (body.classList.contains("light")) {
-        toggleBtn.textContent = "🌙 Dark mode";
-    } else {
-        toggleBtn.textContent = "☀️ Light mode";
-    }
+    document.body.classList.toggle("light");
 });
